a79e709d20d1b67bb85b455efa9cbcc80ef2dbd2
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-04-01 00:32:40 -0400

    hsv work

--- hsv-color-space.py
+++ hsv-color-space.py
@@ -8,15 +8,20 @@ import cv2
 input_image = cv2.imread('./images/rgb.png')
 
 hsv_image = cv2.cvtColor(input_image, cv2.COLOR_BGR2HSV)
-
-cv2.imshow('HSV image', hsv_image)
-cv2.imshow('Hue channel', hsv_image[:, :, 0])
-cv2.imshow('Saturation channel', hsv_image[:, :, 1])
-cv2.imshow('Value channel', hsv_image[:, :, 2])
-
 print("HSV Image shape {}".format(hsv_image.shape))
+print(type(hsv_image))
+print(len(hsv_image[:, :, 0][32]))
+print(len(hsv_image[:, :, 1].shape))
+print(len(hsv_image[:, :, 2].shape))
 
-print(hsv_image)
-
-cv2.waitKey()
-cv2.destroyAllWindows()
+# cv2.imshow('HSV image', hsv_image)
+# cv2.imshow('Hue channel', hsv_image[:, :, 0])
+# cv2.imshow('Saturation channel', hsv_image[:, :, 1])
+# cv2.imshow('Value channel', hsv_image[:, :, 2])
+#
+# print("HSV Image shape {}".format(hsv_image.shape))
+#
+# print(hsv_image)
+#
+# cv2.waitKey()
+# cv2.destroyAllWindows()
